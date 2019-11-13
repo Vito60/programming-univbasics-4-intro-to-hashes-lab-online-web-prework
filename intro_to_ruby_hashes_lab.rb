12e8356256180f3b2f1a03c0323be921b7ed09dd
Randall D Williams <randallw1996@gmail.com>
2019-11-13 20:34:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash[key]
-    
+  read_from_hash= {
+    hash[key]
   }
   # return the correct value using the hash and key parameters
 end
