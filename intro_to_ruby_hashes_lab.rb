573195eb6b854dee5f00e31933e2a4560adb25c5
Randall D Williams <randallw1996@gmail.com>
2019-11-13 20:17:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,8 @@ end
 def my_hash_creator(key, value)
   my_hash_creator = {
     :key => "value"
+    :name => "Grace Hopper"
+    1 => 2
   }
   # return a hash that includes the key and value parameters passed into this method
 end
